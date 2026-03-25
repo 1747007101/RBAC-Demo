fbbbc86299bdd9a2d4f45661d67a91674b11dd95
Mazure <10556213+mazure0215@user.noreply.gitee.com>
2026-03-25 12:38:11 +0800

    asdsad

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,8 +5,23 @@ import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    rules: {
+      "no-unused-vars": "error", // ❌ 未使用变量直接拦
+      "no-console": "warn", // ⚠️ 可选
+      "@typescript-eslint/no-unused-vars": "error", // TS 版本（很关键）
+    },
+  },
 ]);
