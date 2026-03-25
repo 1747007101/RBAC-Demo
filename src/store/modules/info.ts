f8afbfcfec2f0acb89110e3daeb0f66eba9352c0
Mazure <10556213+mazure0215@user.noreply.gitee.com>
2026-03-25 12:59:05 +0800

    feat: 优化

--- src/store/modules/info.ts
+++ src/store/modules/info.ts
@@ -37,7 +37,7 @@ export const useUserStore = defineStore("user", {
         this.setToken(token);
         this.permissions = userInfo.permissions;
         await this.getRoutes();
-        console.log(bb);
+        console.log(bab);
       }
     },
     async logout() {
